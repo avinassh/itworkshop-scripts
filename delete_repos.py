f30512bebcb27281e57e578554881e96d776a748
avinassh <avinashsajjan@gmail.com>
2013-12-19 23:08:39 +0530

    updated delete.py by changing the request url

--- delete_repos.py
+++ delete_repos.py
@@ -3,7 +3,7 @@
 """ 
 This script will delete all the repos of students on the Bitbucket
 
-Input : List of students (name and their email ids), Bitbucket credentials
+Input : List of students ids, Bitbucket credentials
 
 """
 
@@ -15,14 +15,13 @@ from requests.auth import HTTPBasicAuth
 
 BB_USERNAME = 'avinassh'
 BB_PASSWORD = 'ohlongjohnson'
-REPO_API_URL = 'https://api.bitbucket.org/2.0/repositories/'
-INVITE_API_URL = 'https://bitbucket.org/api/1.0/invitations/'
+REPO_API_URL = 'https://api.bitbucket.org/2.0/repositories'
 
 students_info = json.loads(open('students-info.json', 'r').read())
 
 def delete_repo(repo_name):
     auth = HTTPBasicAuth(BB_USERNAME, BB_PASSWORD)
-    url = 'https://api.bitbucket.org/2.0/repositories/'+ BB_USERNAME +'/'+repo_name
+    url = '%s/%s/%s' % (REPO_API_URL, BB_USERNAME, repo_name)
     response = requests.delete(url=url, auth=auth)
     print response.status_code
 
