3d5c7d4c6830f7f5d5482ca12c7edd574f63b187
avinassh <avinashsajjan@gmail.com>
2013-12-19 23:03:07 +0530

    removed invite module, added few comments

--- create_repos.py
+++ create_repos.py
@@ -1,10 +1,10 @@
 #!/bin/python
 
 """ 
-This script will create private repos on Bitbucket and send the invitation 
-email to the all the student 
+This script will create private repositories on Bitbucket for each student.
+The repo name will be same as the university ID of the student.
 
-Input : List of students (name and their email ids), Bitbucket credentials
+Input : List of students ids, Bitbucket credentials
 
 """
 
@@ -17,23 +17,15 @@ from requests.auth import HTTPBasicAuth
 BB_USERNAME = 'avinassh'
 BB_PASSWORD = 'ohlongjohnson'
 REPO_API_URL = 'https://api.bitbucket.org/2.0/repositories/'
-INVITE_API_URL = 'https://bitbucket.org/api/1.0/invitations/'
 
 students_info = json.loads(open('students-info.json', 'r').read())
 
 def create_repo(repo_name):
     auth = HTTPBasicAuth(BB_USERNAME, BB_PASSWORD)
-    url = REPO_API_URL+BB_USERNAME+'/'+repo_name
+    # the request URL format is https://api.bitbucket.org/2.0/repositories/BB_USERNAME/repo_name
+    url = '%s%s/%s' % (REPO_API_URL, BB_USERNAME, repo_name)
     payload = { "scm": "git", "is_private": "true"}
     headers = {'content-type': 'application/json'}
-    response = requests.post(url=url, data=payload, auth=auth)
-    print repo_name, response.status_code
-    #print response.text
-
-def invite_user_to_repo(repo_name, invitee_email):
-    auth = HTTPBasicAuth(BB_USERNAME, BB_PASSWORD)
-    url = INVITE_API_URL+BB_USERNAME+'/'+repo_name+'/'+invitee_email
-    payload = { "permission" : "write" } 
     response = requests.post(url=url, data=payload, auth=auth)
     print repo_name, response.status_code
     #print response.text
@@ -41,7 +33,6 @@ def invite_user_to_repo(repo_name, invitee_email):
 def main():
     for student_id, student_email in students_info.iteritems():
         create_repo(student_id)
-        #invite_user_to_repo(student_id, student_email)
 
 if __name__ == '__main__':
             main()        
