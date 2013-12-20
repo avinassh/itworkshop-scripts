e60eee97e3f6c1bc4e7ad65a7fa63517c326ff76
avinassh <avinashsajjan@gmail.com>
2013-12-20 12:29:18 +0530

    added bitbucket user config file

---
+++ bb_settings.py
@@ -0,0 +1,4 @@
+BB_USERNAME = 'avinassh'
+BB_PASSWORD = 'ohlongjohnson'
+REPO_API_URL = 'https://api.bitbucket.org/2.0/repositories/'
+BB_REPO_BASE_URL = "https://%s:%s@bitbucket.org/%s/" % (BB_USERNAME, BB_PASSWORD, BB_USERNAME)
