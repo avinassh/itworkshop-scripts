1f13949f343de66ac63515dbeb4494e50165ca85
avinassh <avinashsajjan@gmail.com>
2013-12-20 12:21:23 +0530

    added global dirctory config file

---
+++ dir_settings.py
@@ -0,0 +1,3 @@
+MASTER_REPO = '/Users/avi/Desktop/itworkshop/master-repo/'
+STUDENTS_REPO_DIRECTORY = '/Users/avi/Desktop/itworkshop/students-repo-directory/'
+SOLUTIONS_REPO = '/Users/avi/Desktop/itworkshop/solutions-repo/'
