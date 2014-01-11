50bf628e7959e20d97a109070c5f268a161b1355
avinassh <avinashsajjan@gmail.com>
2014-01-11 12:52:00 +0530

    added first version of CSV to json

---
+++ csvtojson.py
@@ -0,0 +1,33 @@
+""" This script will convert the CSV (downloaded from Google docs) to JSON
+in following format 
+
+{
+    "201301007" : "a@vlabs.ac.in",
+    "201301002" : "someguy@vlabs.ac.in",
+    "201301003" : "anotherguy@gmail.com"
+}
+
+and it will save it as student-info.json
+
+usage:
+$python csvtojson.py mycsv.csv
+"""
+
+import sys
+import csv
+import json
+
+
+def csvtojson():
+    data = {}
+    for row_no, student_id, student_name, student_email in csv.reader(open(sys.argv[1])):
+        data[student_id] = student_email
+    f = open('student-info.json', 'w+') 
+    f.write(json.dumps(data))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.stderr.write('usage:\ncsvtojson <filepath>\n')
+        sys.exit()
+    csvtojson()
